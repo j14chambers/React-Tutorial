0d53c4c2824a557ea4353d6490e91077f7157479
jay14chambers <jessi14chambers@gmail.com>
2019-11-01 18:24:19 -0500

    Add header and addtodo components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
 
-// function App() {
-//   state = {
-//     todos: [
-//       {
-//         id: 1,
-//         title: 'Take out trash',
-//         completed: false
-//       },
-//       {
-//         id: 2,
-//         title: 'Bus',
-//         completed: false
-//       },
-//       {
-//         id: 3,
-//         title: 'Check tut',
-//         completed: false
-//       }
-//     ]
-//   }
-//   console.log(this.state.todos);
-//   return (
-//     <div className="App">
-//      <h1>Hello</h1>
-//      <Todos></Todos>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   state = {
@@ -71,8 +45,12 @@ class App extends Component {
     console.log(this.props.todos);
     return(
       <div className="App">
-        <h1>Hello</h1>
-        <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}></Todos>
+        <div className='container'>
+          <Header></Header>
+          <AddTodo></AddTodo>
+          <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}></Todos>
+        </div>
+        
      </div>
     )
   }
